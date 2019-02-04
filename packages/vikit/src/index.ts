a2412549926b9aa510584c35ac1fdf7ebd19564e
Phạm Dương Nhật Nam <nampdn@protonmail.com>
2019-02-04 15:08:49 +0000

    fix(vikit): export changePhonePrefix to root

--- packages/vikit/src/index.ts
+++ packages/vikit/src/index.ts
@@ -1,1 +1,2 @@
 export * from './diacritic'
+export * from './phone-prefix'
--- packages/vikit/src/phone-prefix.ts
+++ packages/vikit/src/phone-prefix.ts
@@ -1,7 +1,12 @@
 const OLD_PREFIX = ['0120', '0121', '0122', '0126', '0128', '0123', '0124', '0125', '0127', '0129', '0162', '0163', '0164', '0165', '0166', '0167', '0168', '0169', '0186', '0188', '0199'];
 const NEW_PREFIX = ['070', '079', '077', '076', '078', '083', '084', '085', '081', '082', '032', '033', '034', '035', '036', '037', '038', '039', '056', '058', '059'];
 
-export const changePhonePrefix = (phoneNumber: string) => {
+/**
+* Change phone prefix with the new rules.
+* @param {string} phoneNumber Which phone number to change.
+* @return {string} Changed phone number or the same if not in the change list.
+*/
+export const changePhonePrefix = (phoneNumber: string): string => {
   const prefix = phoneNumber.substr(0, 4);
   const i = OLD_PREFIX.indexOf(prefix);
   if (i > -1) {
